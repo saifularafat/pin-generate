84239f3b42d49a2e0952842fa8d0787d9946b052
saifularafat <saifularafat.info@gmail.com>
2023-02-16 23:59:25 +0600

    Modefiy The ifelse

--- js/pin.js
+++ js/pin.js
@@ -29,6 +29,11 @@ document.getElementById('btn-genetate-pin').addEventListener('click', function()
 
 
 document.getElementById('calculater').addEventListener('click', function(event){
+    // thre time try korar pore input field none hoiya jabe
+    const errorThreTime = document.getElementById('error-3time').innerText;
+    if(parseInt(errorThreTime) >= 0){
+        return;
+    }
 
     const number = event.target.innerText;
     const typeNumberField = document.getElementById('type-field');
@@ -69,6 +74,15 @@ document.getElementById('btn-submit').addEventListener('click', function(){
         faildMessage.style.display = 'none';
     }
     else{
+         
+        let errorHandler = document.getElementById('error-3time');
+
+        if(parseInt(errorHandler.innerText) <= 0){
+            document.getElementById('type-field').setAttribute('disabled', true);
+            return;
+        }
+        errorHandler.innerText = parseInt(errorHandler.innerText) - 1;
+
         faildMessage.style.display = 'block';
         successMessage.style.display = 'none';
     }
